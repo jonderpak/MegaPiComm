dc939e35ffed32f2a9f3e6a97f0930009fdea75b
Jonathon Derpak <124627357+jonderpak@users.noreply.github.com>
2026-05-29 09:49:57 -0700

    Refactor MegaPiComm for receive handling and config
    Updated MegaPiComm class to include receive handler and ignore whitespace option.

--- src/MegaPiComm.h
+++ src/MegaPiComm.h
@@ -4,55 +4,43 @@
 
 class MegaPiComm;
 
-using MegaPiCommandHandler = void (*)(MegaPiComm &bridge, HardwareSerial &sourcePort, const __FlashStringHelper *sourceName, char command);
-using MegaPiUnknownCommandHandler = bool (*)(MegaPiComm &bridge, HardwareSerial &sourcePort, const __FlashStringHelper *sourceName, char command);
+using MegaPiRxHandler = void (*)(MegaPiComm &bridge, HardwareSerial &sourcePort, const __FlashStringHelper *sourceName, char incoming);
 
 struct MegaPiCommConfig {
   unsigned long usbBaud = 115200;
-  unsigned long bluetoothBaud = 115200;  // Informational input; bridge uses fixed BLE baud.
+  unsigned long bluetoothBaud = 115200;  // Informational input; transport uses fixed BLE baud.
   bool verboseRxLogging = false;
-  char helpCommand = '?';
-  char statusCommand = 'B';
+  bool ignoreWhitespace = true;
 };
 
 class MegaPiComm {
 public:
   static constexpr unsigned long kFixedBluetoothBaud = 115200;
-  static constexpr uint8_t kMaxCommands = 16;
 
   explicit MegaPiComm(const MegaPiCommConfig &config = MegaPiCommConfig());
 
   void begin();
   void poll();
 
-  bool setCommandHandler(char command, MegaPiCommandHandler handler);
-  void clearCommandHandlers();
-  void setUnknownCommandHandler(MegaPiUnknownCommandHandler handler);
+  void setReceiveHandler(MegaPiRxHandler handler);
 
-  void setHelpText(const __FlashStringHelper *helpText);
-  void printHelp(HardwareSerial &port);
-  void printStatus(HardwareSerial &port);
+  bool hasActiveBlePort() const;
+  HardwareSerial *activeBlePort();
 
+  void sendChar(HardwareSerial &port, char value);
+  void sendText(HardwareSerial &port, const char *message);
+  void sendLine(HardwareSerial &port, const char *message);
   void sendLine(HardwareSerial &port, const __FlashStringHelper *message);
-  void sendAck(HardwareSerial &sourcePort, const __FlashStringHelper *sourceName, const __FlashStringHelper *message);
+
+  const __FlashStringHelper *sourceLabel(HardwareSerial &port) const;
 
 private:
-  struct CommandBinding {
-    char command;
-    MegaPiCommandHandler handler;
-  };
-
-  char normalizeCommand(char cmd) const;
   const __FlashStringHelper *portLabel(HardwareSerial &port) const;
-  void reportCommandSource(char cmd, const __FlashStringHelper *sourceName);
   void logRawByte(const __FlashStringHelper *portName, uint8_t incoming);
-  void handleCommand(char cmd, HardwareSerial &sourcePort, const __FlashStringHelper *sourceName);
   void pollSerialPort(HardwareSerial &port, const __FlashStringHelper *portName);
 
   MegaPiCommConfig config_;
-  CommandBinding commandBindings_[kMaxCommands];
-  uint8_t commandCount_;
-  MegaPiUnknownCommandHandler unknownCommandHandler_;
-  const __FlashStringHelper *helpText_;
+  MegaPiRxHandler rxHandler_;
   bool bluetoothPortDetected_;
+  HardwareSerial *activeBlePort_;
 };
